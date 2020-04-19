263987f27f6b34f24be67d821d7b3b8ec0c7e1e4
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-04-19 12:06:59 +0200

    First Component

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,23 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Liste from "./Liste";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Liste />
       </header>
     </div>
   );
---
+++ first-app/src/Liste.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Liste extends Component {
+
+    render() {
+        return (
+            <h1>Salut les amis</h1>
+
+        )
+    }
+ }
+
+ export default Liste;
