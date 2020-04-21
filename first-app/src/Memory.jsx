a7da39ca26a9c43d1a55564c7e0630f598beb7aa
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-04-21 17:42:56 +0200

    On peut retourner les cartes une par une

--- first-app/src/Memory.jsx
+++ first-app/src/Memory.jsx
@@ -9,6 +9,7 @@ import Grid from "@material-ui/core/Grid";
 import bg from "./images/bg.png";
 
 import image1 from "./images/image1.jpg";
+import { CardContent } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
@@ -22,260 +23,55 @@ const useStyles = makeStyles({
 
 export default function MediaCard() {
   const classes = useStyles();
-  const [isFlip, setFlip] = useState(false);
+  const [cards, setCards] = useState(shuffle());
 
+  function shuffle() {
+    let cards = [];
+    for (let i = 0; i < 12; i++) {
+      cards[i] = {
+        id: i,
+        pairId: i % 2 === 0 ? i+1 : i-1,
+        solved: false,
+        isFlip: false,
+      };
+    }
+    cards.sort(() => Math.random() - 0.5);
+    return cards;
+  }
 
+  const handleClic = (id) => {
+    const c = [...cards];
+    c.find((card) => {
+      if (card.id === id) {
+        card.isFlip = card.isFlip ? false : true;
+        return true;
+      }
+      return false;
+    });
+
+    setCards(c);
+  };
 
   return (
     <div>
-    
-    <Grid container>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-        <Flip right spy={isFlip}>
-            <Card className={classes.root}>
-                <CardMedia className={classes.media} image={isFlip ? image1 : bg} onClick={() => setFlip(!isFlip)} />
-            </Card>
-        </Flip>
-
-    </Grid>
-    
-      
+      <Grid container>
+        {cards.map((card) => {
+          return (
+            <div>
+              <Flip right spy={card.isFlip}>
+                <Card className={classes.root}>
+                  <CardMedia
+                    className={classes.media}
+                    image={card.isFlip ? image1 : bg}
+                    onClick={() => handleClic(card.id)}
+                  />
+                  <CardContent>{card.id} -> {card.pairId}</CardContent>
+                </Card>
+              </Flip>
+            </div>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
-
-
